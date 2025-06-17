05035ac0a324a4a4c626902753e4802fb6cef881
Karl Schriek <karl.schriek@bma.network>
2025-06-17 18:14:34 +0200

    Update main.tf

--- getting-started/four-module-dag/module1/main.tf
+++ getting-started/four-module-dag/module1/main.tf
@@ -3,3 +3,8 @@ resource random_pet  module1 {}
 output module1_pet {
     value = random_pet.module1.id
 }
+
+output module1_pet_sensitive {
+    sensitive = true
+    value = random_pet.module1.id
+}
